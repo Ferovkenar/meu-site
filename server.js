84186c424cb77f2646830017464809b3adcdf9af
Ferovkenar <119982780+Ferovkenar@users.noreply.github.com>
2024-06-25 15:08:47 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
+// Permitir requisições do frontend hospedado no GitHub Pages
+app.use(cors({ origin: 'https://github.com/Ferovkenar/meu-site.git' }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static('public'));
+
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
+// Rotas para registro, login e home
 app.get('/register', (req, res) => res.render('register'));
 app.get('/login', (req, res) => res.render('login'));
-app.get('/index', (req, res) => res.render('index'));
+app.get('/home', (req, res) => res.render('home'));
 
 app.use('/auth', authRoutes);
 
