7795305368420cdc6bb92aeb4016ef8f2614bbb2
Ferovkenar <119982780+Ferovkenar@users.noreply.github.com>
2024-06-25 15:11:10 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,10 +4,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    host: 192.168.100.70,
+    user: root,
+    password: Ferovkenar132@,
+    database: user_auth
 });
 
 module.exports = pool.promise();
